6ee9416cd9390c4a40ba11cd8331f3015e33d552
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-11-02 15:04:54 -0300

    add requisito 6

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,7 +7,7 @@ class PlanetCard extends React.Component {
     const { planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <h2 data-testid="planet-name">{planetName}</h2>
+        <h3 data-testid="planet-name">{planetName}</h3>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
